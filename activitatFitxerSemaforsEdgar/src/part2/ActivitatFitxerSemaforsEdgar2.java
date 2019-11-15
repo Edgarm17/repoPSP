9dd7a6d9e83fb4c27cc13fe4e84a6c2e39fb17a5
Edgar <edgaralkugm@gmail.com>
2019-11-15 20:31:01 +0100

    SemaforsLinies

--- activitatFitxerSemaforsEdgar/src/part2/ActivitatFitxerSemaforsEdgar2.java
+++ activitatFitxerSemaforsEdgar/src/part2/ActivitatFitxerSemaforsEdgar2.java
@@ -3,8 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package activitatfitxersemaforsedgar;
+package part2;
 
+import activitatfitxersemaforsedgar.*;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -15,108 +16,119 @@ import java.io.PrintWriter;
 import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 /**
  *
  * @author Edgar
  */
-public class ActivitatFitxerSemaforsEdgar {
-    
+public class ActivitatFitxerSemaforsEdgar2 {
+
     private static File arxiu = new File("linies.txt");
-    
-    private static final Semaphore MUTEX = new Semaphore(0);
 
     
-    public static void main(String[] args) {
+    private static final Semaphore MUTEX = new Semaphore(1);
+    private static final Semaphore PARELL_ESCRIT = new Semaphore(0);
+    private static final Semaphore IMPARELL_ESCRIT = new Semaphore(1);
+
+    public static void main(String[] args) throws IOException {
         proces1 p1 = new proces1();
         proces2 p2 = new proces2();
-        
+
         p1.start();
         p2.start();
     }
-    
-    static class proces1 extends Thread{
-        
+
+   
+
+    static class proces1 extends Thread {
+
         private File nouArxiu = new File("linescanviades.txt");
-        private int cont = 1;
-        public proces1(){
-            
+
+        public proces1() {
+
         }
-        
-        
-        public void run(){
+
+        public void run() {
             try {
                 
                 System.out.println("Proces1");
                 FileReader fr = new FileReader(arxiu);
                 BufferedReader br = new BufferedReader(fr);
-                
-                FileWriter fw = new FileWriter(nouArxiu,true);
+
+                FileWriter fw = new FileWriter(nouArxiu, true);
                 PrintWriter pw = new PrintWriter(fw);
-                
+
                 String linea = br.readLine();
-                while (linea!= null) {
+                while (linea != null) {
+                    PARELL_ESCRIT.acquire();
+                    MUTEX.acquire();
+                    System.out.println(linea);
                     
-                    if (cont%2 != 0) {
-                        pw.write(linea+"\n");
-                    }
-                    cont++;
+                    pw.write(linea + "\n");
+                    
+                    
                     linea = br.readLine();
+                    MUTEX.release();
+                    IMPARELL_ESCRIT.release();
                 }
-                
+
                 br.close();
                 pw.close();
-                MUTEX.release();
                 
-            } catch (FileNotFoundException ex) {
-                System.err.println("Arxiu no trobat");
-            } catch (IOException ex) {
-                System.out.println("final");
-            }
-        }
-    }
-    
-    static class proces2 extends Thread{
-        
-        private File nouArxiu = new File("linescanviades.txt");
-        private int cont = 1;
-        
-        public proces2(){
-            
-        }
-        
-        
-        public void run(){
-            try {
-                MUTEX.acquire();
-                System.out.println("Proces 2");
-                FileReader fr = new FileReader(arxiu);
-                BufferedReader br = new BufferedReader(fr);
-                
-                FileWriter fw = new FileWriter(nouArxiu,true);
-                PrintWriter pw = new PrintWriter(fw);
-                
-                String linea = br.readLine();
-                while (linea!= null) {
-                    if (cont%2 == 0) {
-                        pw.write(linea+"\n");
-                    }
-                    cont++;
-                    linea = br.readLine();
-                }
-                
-                br.close();
-                pw.close();
-                MUTEX.release();
+
             } catch (FileNotFoundException ex) {
                 System.err.println("Arxiu no trobat");
             } catch (IOException ex) {
                 System.out.println("final");
             } catch (InterruptedException ex) {
-                Logger.getLogger(ActivitatFitxerSemaforsEdgar.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(ActivitatFitxerSemaforsEdgar2.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
         }
     }
-    
+
+    static class proces2 extends Thread {
+
+        private File nouArxiu = new File("linescanviades.txt");
+
+        public proces2() {
+
+        }
+
+        public void run() {
+            try {
+
+                
+                System.out.println("Proces 2");
+                FileReader fr = new FileReader(arxiu);
+                BufferedReader br = new BufferedReader(fr);
+
+                FileWriter fw = new FileWriter(nouArxiu, true);
+                PrintWriter pw = new PrintWriter(fw);
+
+                String linea = br.readLine();
+                while (linea != null) {
+                    IMPARELL_ESCRIT.acquire();
+                    MUTEX.acquire();
+                    System.out.println(linea);
+                    pw.write(linea + "\n");
+                    linea = br.readLine();
+                    MUTEX.release();
+                    PARELL_ESCRIT.release();
+                }
+
+                br.close();
+                pw.close();
+                
+            } catch (FileNotFoundException ex) {
+                System.err.println("Arxiu no trobat");
+            } catch (IOException ex) {
+                System.out.println("final");
+            } catch (InterruptedException ex) {
+                Logger.getLogger(ActivitatFitxerSemaforsEdgar2.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+        }
+    }
+
 }
